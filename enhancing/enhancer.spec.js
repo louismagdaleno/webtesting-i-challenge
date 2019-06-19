a636f97ddd4abbb3d36c834613d7c3b452addfb3
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-19 13:39:59 -0700

    Added logic to not increment enhancement if already 20

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -16,11 +16,19 @@ describe('succeed', () => {
         expect(enhancer.succeed).toBeDefined();
     });
 
-    it('returns a valid item with increased enhancement', () => {
+    it('does increase enhnacement when value is less than 20', () => {
         let item = { enhancement : 19 };
         let expected = 20;
         let actual = enhancer.succeed(item).enhancement;
 
         expect(actual).toBe(expected);
     });
+
+    it('does not increment enhnacement when value is already 20 or up', () => {
+        let item = { enhancement: 20};
+        let expected = 20;
+        let actual = enhancer.succeed(item).enhancement;
+
+        expect(actual).toBe(expected);
+    })
 });
